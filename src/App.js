8ca7c28772dd0efd0c0cf2b815c7bc007a775b3f
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-06-28 19:45:40 -0400

    waiting for conflict

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 function App() {
-  return (
-    <div>
-      <div className="App">
-        <header className="header">
-          <h2>Welcome to your git exercise. Good luck and have fun!</h2>
-        </header>
-      </div>
+   return (
       <div>
-        <h3>Hello There, General Kenobi</h3>
-        <a href="https://starwars.fandom.com/wiki/Grogu" target="_blank">
-          <button> jedi mind trick </button>
-        </a>
-        <div>
-          {' '}
-          <img
-            src="https://static.wikia.nocookie.net/starwars/images/b/bf/Vaders_Castle_FDKF.png"
-            alt="the dark side"
-          ></img>{' '}
-        </div>
+         <div className="App">
+            <header className="header">
+               <h2>Welcome to your git exercise. Good luck and have fun!</h2>
+            </header>
+         </div>
+         <div>
+            <h3>Hello There, General Kenobi!!!!!</h3>
+            <a href="https://starwars.fandom.com/wiki/Grogu" target="_blank">
+               <button> jedi mind trick </button>
+            </a>
+            <div>
+               {' '}
+               <img
+                  src="https://static.wikia.nocookie.net/starwars/images/b/bf/Vaders_Castle_FDKF.png"
+                  alt="the dark side"
+               ></img>{' '}
+            </div>
+         </div>
+         <div>
+            <h1>The force is strong with this one</h1>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default App;
