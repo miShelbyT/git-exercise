77cb2e7e2a921612c6842ee17ec689541866bd11
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-06-28 18:52:05 -0400

    Added div

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <h2>Welcome to your git exercise. Good luck and have fun!</h2>
-      </header>
+    <div>
+      <div className="App">
+        <header className="header">
+          <h2>Welcome to your git exercise. Good luck and have fun!</h2>
+        </header>
+      </div>
+      <div>
+        <h3>Hello There, General Kenobi</h3>
+      </div>
     </div>
   );
 }
